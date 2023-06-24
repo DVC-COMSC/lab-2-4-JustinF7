2124a7ae723c423ca881c0b156c9b5a926112eae
JustinF7 <fu.justin3@gmail.com>
2023-06-24 14:38:27 -0700

    main

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ def main():
     sub1 = original_str[0: 6]
     sub2 = original_str[7: 18]
     merge_str = sub2 + ' ' + sub1
+    merged_str = merge_str
     print(sub1)
     print(sub2)
-    print(merge_str)
+    print(merged_str)
     pass
 
 
