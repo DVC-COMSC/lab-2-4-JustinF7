85fe23804cbe1f4c126937035f8473a91b0f2429
JustinF7 <fu.justin3@gmail.com>
2023-06-24 14:37:01 -0700

    main

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 def main():
     ##################################################
-    # Comlete your code here
+    # Complete your code here
     ##################################################
     original_str = "Python Programming"
     sub1 = original_str[0: 6]
     sub2 = original_str[7: 18]
-    merged_str = sub2 + ' ' + sub1
+    merge_str = sub2 + ' ' + sub1
     print(sub1)
     print(sub2)
-    print(merged_str)
+    print(merge_str)
     pass
 
 
